a4af649fb06b897ab3a27d6ef76926cf9e85d921
username <user@email.mail>
2020-02-17 15:54:49 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="loog" />
+      <h2>Hello World!</h2>
+    </div>
+  );
+}
+
+export default App;
